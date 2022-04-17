0bf455be648deb47474b9175e63cdce5b49657c7
Nathnael Getahun Woldemariam <contact.nathnael@gmail.com>
2022-04-18 00:42:10 +0300

    Implemented card drag dropping

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -25,6 +25,15 @@ type Action =
             hoverIndex: number
         }
     }
+    | {
+        type: "MOVE_TASK"
+        payload: {
+            dragIndex: number
+            hoverIndex: number
+            sourceColumn: string
+            targetColumn: string
+        }
+    }
     
 
 interface AppStateContextProps {
@@ -100,6 +109,19 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
         }
         case "SET_DRAGGED_ITEM": {
             return { ...state, draggedItem: action.payload }
+        }
+        case "MOVE_TASK": {
+            const {
+                dragIndex,
+                hoverIndex,
+                sourceColumn,
+                targetColumn
+            } = action.payload
+            const sourceLaneIndex = findItemIndexById(state.lists, sourceColumn)
+            const targetLaneIndex = findItemIndexById(state.lists, targetColumn)
+            const item = state.lists[sourceLaneIndex].tasks.splice(dragIndex, 1)[0]
+            state.lists[targetLaneIndex].tasks.splice(hoverIndex, 0, item)
+            return { ...state }
         }
         default: {
             return state
